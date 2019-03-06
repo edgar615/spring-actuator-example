e8f98e1b45364e41902b4588599454a1a73ddc06
edgar615 <edgar615@gmail.com>
2019-03-06 23:41:19 +0800

    expose, enable

---
+++ expose/src/main/java/com/github/edgar615/spring/example/actuator/ExposeApplication.java
@@ -0,0 +1,12 @@
+package com.github.edgar615.spring.example.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExposeApplication.class, args);
+  }
+}
